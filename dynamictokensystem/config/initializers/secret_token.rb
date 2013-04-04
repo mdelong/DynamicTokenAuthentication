13df30dc17b93d94d6469af81526a5b94b073112
Juan Moreno <cyberjuan90@gmail.com>
2013-04-04 19:35:27 -0400

    basic rails app scaffolded

---
+++ dynamictokensystem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dynamictokensystem::Application.config.secret_token = '7816c72574f7ed36fe50ddedf4830931983fc2f46fa88ee53fb9b83b98152340a383cce7bb0ce5a5fc1bac41ebbf86060e0474555328159ae42ee5180e635efd'
